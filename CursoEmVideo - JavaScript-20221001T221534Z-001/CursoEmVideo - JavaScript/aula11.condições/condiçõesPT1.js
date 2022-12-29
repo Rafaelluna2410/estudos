a393a750b43199509f89058d2e8fc8a7dae85cbc
root <rafael.luna.cavalcanti@gmail.com>
2022-12-29 16:32:23 -0300

    organização

--- CursoEmVideo - JavaScript-20221001T221534Z-001/CursoEmVideo - JavaScript/aula11.condições/condiçõesPT1.js
+++
@@ -1,9 +0,0 @@
-var vel = 12
-
-console.log(`A velocidade do seu carro é ${vel} km/h`)
-
-if (vel > 60) {  //condição simples
-    console.log("Você ultrpassou a velocidade permitida. MULTADO!")
-
-}
-{console.log("Dirija sempre assim")}
