5440f83773569be19637d87943452e1bae2b7f46
rafael <rafael.luna.cavalcanti@gmail.com>
2023-05-22 15:05:21 -0300

    upload arquivos

---
+++ dockerFileEstudo/exemplo-6/teste.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export MYSQL_DATABASE=nextcloud5
+export MYSQL_USER=miguel
+export MYSQL_PASSWORD=miguel
+echo "rodou"
+
+#utilizar source ./ nome arquivo    para poder exportar do shell atual para outros shels e para o docker poder usar
