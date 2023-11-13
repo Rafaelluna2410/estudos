7b4142214994912bab0b02bd566be38a3f7e4d26
rafael luna <rafael.luna.cavalcanti@gmail.com>
2023-11-13 19:57:40 -0300

    aba para login

--- ifpe/web-1/projeto-2-unidade/script.js
+++ ifpe/web-1/projeto-2-unidade/script.js
@@ -1,36 +1,14 @@
-/* let locais = [".jamaica", ".bahamas", ".butao"];
-let cont = 0;
 
-function mostrar() {
-    for (let i = 0; i < locais.length; i++) {
-        let element = document.querySelector(locais[i]);
-        element.style.display = i === cont ? "block" : "none";
-    }
 
-    cont = (cont + 1) % locais.length;
-
-    setTimeout(mostrar, 10000);
+document.getElementById('login_butao').addEventListener('click', function () {
+  document.getElementById('loginModal').style.display = 'block';
+});
+function fecharModal() {
+  document.getElementById('loginModal').style.display = 'none';
 }
-mostrar();
- */
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+window.onclick = function (event) {
+  if (event.target == document.getElementById('loginModal')) {
+    document.getElementById('loginModal').style.display = 'none';
   }
-  
-  slideIndex++;
-  
-  if (slideIndex > slides.length) { 
-    slideIndex = 1 
-  }
-  
-  slides[slideIndex - 1].style.display = "block";  
-  setTimeout(showSlides, 2000); // Mude 2000 para a quantidade desejada de milissegundos entre os slides
-}
+};
