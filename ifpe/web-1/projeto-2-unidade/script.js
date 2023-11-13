22ea270e893d3e2bf8c51339bdb91bcceb241276
rafael luna <rafael.luna.cavalcanti@gmail.com>
2023-11-13 14:38:23 -0300

    background slide // falta ajeitar a posição dele

--- ifpe/web-1/projeto-2-unidade/script.js
+++ ifpe/web-1/projeto-2-unidade/script.js
@@ -1,18 +1,36 @@
-let locais = [".jamaica", ".bahamas", ".butao"];
-
-let jamaica = document.querySelector(locais[0]);
-let bahamas = document.querySelector(locais[1]);
-let butao = document.querySelector(locais[2]);
-
-jamaica.style.display = "none";
-bahamas.style.display = "none";
-butao.style.display = "none";
+/* let locais = [".jamaica", ".bahamas", ".butao"];
+let cont = 0;
 
 function mostrar() {
     for (let i = 0; i < locais.length; i++) {
         let element = document.querySelector(locais[i]);
-        element.style.display = "block";
+        element.style.display = i === cont ? "block" : "none";
     }
-}
 
-setTimeout(mostrar, 5000);
+    cont = (cont + 1) % locais.length;
+
+    setTimeout(mostrar, 10000);
+}
+mostrar();
+ */
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  slideIndex++;
+  
+  if (slideIndex > slides.length) { 
+    slideIndex = 1 
+  }
+  
+  slides[slideIndex - 1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Mude 2000 para a quantidade desejada de milissegundos entre os slides
+}
