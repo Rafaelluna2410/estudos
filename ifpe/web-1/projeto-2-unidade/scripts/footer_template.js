c4047fe4a0e740bfab9f0e2617abbdf4cefddf86
rafael luna <rafael.luna.cavalcanti@gmail.com>
2023-11-18 03:33:39 -0300

    kriado templates footer andar header e adikionado sobre.html usando template

---
+++ ifpe/web-1/projeto-2-unidade/scripts/footer_template.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadFooter();
+});
+function loadFooter() {
+
+    fetch('/html_templates/footer.html')
+        .then(response => response.text())
+        .then(html => {
+            var container = document.getElementById('footerContainer');
+            if (container) {
+                container.innerHTML = html;
+            }
+        })
+        .catch(error => console.error('Error loading footer:', error));
+}
---
+++ ifpe/web-1/projeto-2-unidade/scripts/header-nav_template.js
@@ -0,0 +1,32 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadHeaderAndNav();
+});
+
+function loadHeaderAndNav() {
+    fetch('/html_templates/header-nav.html')
+        .then(response => response.text())
+        .then(html => {
+        
+            var container = document.getElementById('headerAndNavContainer');
+            if (container) {
+                container.innerHTML = html;
+                document.getElementById('login_butao').addEventListener('click', function () {
+                    document.getElementById('loginModal').style.display = 'block';
+                  });
+                  function fecharModal() {
+                    document.getElementById('loginModal').style.display = 'none';
+                  }
+                  
+                  window.onclick = function (event) {
+                    if (event.target == document.getElementById('loginModal')) {
+                      document.getElementById('loginModal').style.display = 'none';
+                    }
+                  };
+            }
+        })
+        .catch(error => console.error('Error loading header and navigation:', error));
+}
+loginButton.addEventListener('click', function () {
+    abrirModal();
+});
